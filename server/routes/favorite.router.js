b5318a44ab440c6ae58a0ef337ffacfd2082f367
Chaoching Vang <chaoching.vang@gmail.com>
2021-11-04 13:49:49 -0500

    created favorite.router get and post request

--- server/routes/favorite.router.js
+++ server/routes/favorite.router.js
@@ -5,12 +5,41 @@ const router = express.Router();
 
 // return all favorite images
 router.get('/', (req, res) => {
-  res.sendStatus(200);
+  console.log(`/favorite GET`);
+
+  let queryText = `SELECT * FROM "SONGS";`;
+
+  pool.query(queryText)
+    .then((result) => {
+      res.send(result.rows);
+    })
+    .catch((err) => {
+      console.log(`ERROR! /favorite GET`);
+      res.sendStatus(500);
+    })
 });
 
 // add a new favorite
 router.post('/', (req, res) => {
-  res.sendStatus(200);
+  console.log(`/favorite POST`);
+  const newGif = req.body;
+
+  let queryText = `
+    INSERT INTO "favorites" ("name", "url")
+    VALUES ($1, $2);
+  `;
+  let values = [newGif.name, newGif.image_url]
+
+
+  pool.query(queryText, values)
+    .then((res) => {
+      console.log(`/favorites POST request SUCCESS!`);
+      res.sendStatus(201);
+    })
+    .catch((err) => {
+      console.log(`/favorites POST request ERROR!`);
+      res.sendStatus(500);
+    })
 });
 
 // update given favorite with a category id
