8a7f0d185edee97cb5898de3843395ef8a90f7c4
Julia Bayless <julia.allistair@gmail.com>
2021-11-04 14:06:04 -0500

    GihyLIst done - need to do item

--- src/components/GiphyList/GiphyList.jsx
+++ src/components/GiphyList/GiphyList.jsx
@@ -1,8 +1,18 @@
+import { useSelector } from 'redux'
+import GiphyItem from '../GiphyItem/GiphyItem';
+
 function GiphyList() {
+  //hook to grab the data in from Fav DB
+  const gifList = useSelector(store => store.gifReducer)
+  
   return (
     <>
       <h2>GiphyList</h2>
-      
+        <div className="gifContainer">
+        {gifList.map((gif)=> (
+          <GiphyItem gif={gif} />
+        ))}
+        </div>
     </>
   );
 }
