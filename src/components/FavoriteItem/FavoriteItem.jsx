dbde892886bf9178c0ae93bc7c76eb0e788641c2
Hamsa Alimci <alimici2016@gmail.com>
2021-11-04 14:30:44 -0500

    Added the POST, PUT, DELETE

--- src/components/FavoriteItem/FavoriteItem.jsx
+++ src/components/FavoriteItem/FavoriteItem.jsx
@@ -1,11 +1,14 @@
+import { useDispatch } from "react-redux";
 function FavoriteItem({gif}) {
+
+    const dispatch = useDispatch();
     return(
         <>
         <p>FavoriteItem</p>
         <li>
             {gif.name}
             <img title={gif.title} src={gif.image_url} />
-            <button onClick>Delete Gif</button>
+            <button onClick={()=> dispatch({type:'DELETE_GIF', payload: gif.id})}>Delete Gif</button>
          </li>
         </>
     )
