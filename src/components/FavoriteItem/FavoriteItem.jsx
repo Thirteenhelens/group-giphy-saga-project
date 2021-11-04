38bb719a4f5aca1a0b4fe7b872740ca592d990cc
Hamsa Alimci <alimici2016@gmail.com>
2021-11-04 13:46:05 -0500

    The list is mapped through and we rendered delete buttons

--- src/components/FavoriteItem/FavoriteItem.jsx
+++ src/components/FavoriteItem/FavoriteItem.jsx
@@ -1,7 +1,12 @@
-function FavoriteItem() {
+function FavoriteItem({gif}) {
     return(
         <>
         <p>FavoriteItem</p>
+        <li>
+            {gif.name}
+            <img title={gif.title} src={gif.image_url} />
+            <button onClick>Delete Gif</button>
+         </li>
         </>
     )
 }
