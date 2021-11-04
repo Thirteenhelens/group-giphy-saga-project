6c7719cc4faae56ce77fd5dd03326a2b03f8a95c
Julia Bayless <julia.allistair@gmail.com>
2021-11-04 14:27:54 -0500

    item bug fixed

--- src/components/GiphyList/GiphyList.jsx
+++ src/components/GiphyList/GiphyList.jsx
@@ -3,16 +3,18 @@ import GiphyItem from '../GiphyItem/GiphyItem';
 
 function GiphyList() {
   //hook to grab the data in from Fav DB
-  const gifList = useSelector(store => store.gifReducer)
-  
+  const searchGifList = useSelector(store => store.searchGifReducer)
+
   return (
     <>
       <h2>GiphyList</h2>
-        <div className="gifContainer">
-        {gifList.map((gif)=> (
+     
+        {searchGifList.map((gif) => (
+        <div key={gif.id} className="gifContainer">
           <GiphyItem gif={gif} />
-        ))}
-        </div>
+          </div>
+       ))}
+     
     </>
   );
 }
