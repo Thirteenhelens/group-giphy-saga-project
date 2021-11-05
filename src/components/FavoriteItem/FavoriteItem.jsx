048546c8c766427a4ff1fb6c9b10bf3a5228c27c
Jack Barrett <jbarrett1347@gmail.com>
2021-11-05 12:01:57 -0500

    Added MUI styling

--- src/components/FavoriteItem/FavoriteItem.jsx
+++ src/components/FavoriteItem/FavoriteItem.jsx
@@ -1,28 +1,47 @@
+import "./FavoriteItem.css";
+import { useEffect } from "react";
+import Card from "@mui/material/Card";
 import { useDispatch } from "react-redux";
-import { useEffect } from 'react';
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import DeleteTwoToneIcon from "@mui/icons-material/DeleteTwoTone";
 
 function FavoriteItem({ gif }) {
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        fetchGif();
-    }, []);
+  useEffect(() => {
+    fetchGif();
+  }, []);
 
-    const fetchGif = () => {
-        dispatch({ type: 'FETCH_GIF' })
-    }
+  const fetchGif = () => {
+    dispatch({ type: "FETCH_GIF" });
+  };
 
-    const dispatch = useDispatch();
-
-    console.log(`GIFFFF`, gif);
-    return (
-        <>
-            <p>FavoriteItem</p>
-            <li>
-                <img src={gif?.image_url}/>
-                <button onClick={()=> dispatch({type:'DELETE_GIF', payload: gif.id})}>Delete Gif</button>
-            </li>
-        </>
-    )
+  return (
+    <div className="favGifItem">
+      <Card sx={{ maxWidth: 345 }}>
+        <CardMedia component="img" height="350" image={gif?.image_url} />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {gif?.name}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <IconButton
+            size="small"
+            aria-label="delete"
+            onClick={() => dispatch({ type: "DELETE_GIF", payload: gif.id })}
+          >
+            <DeleteTwoToneIcon />
+          </IconButton>
+          <Typography>Delete</Typography>
+        </CardActions>
+      </Card>
+    </div>
+  );
 }
 
 export default FavoriteItem;
--- src/components/GiphyItem/GiphyItem.jsx
+++ src/components/GiphyItem/GiphyItem.jsx
@@ -1,31 +1,59 @@
+import "./GiphyItem.css";
+import { useState } from "react";
+import Card from "@mui/material/Card";
 import { useDispatch } from "react-redux";
-
-
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
 
 function GiphyItem({ gif }) {
-
   const dispatch = useDispatch();
+  const [clicked, setClicked] = useState(false);
 
   const handleSubmitFav = () => {
     //when fav button clicked, dispatch to post url
-    //**payload may need to change depending on how data 
+    //**payload may need to change depending on how data
     //comes from the API
+    setClicked(!clicked);
     dispatch({
-      type: 'ADD_GIF',
+      type: "ADD_GIF",
       payload: {
         name: gif.title,
-        image_url: gif.images.fixed_height.url
-      }
-    })
-  }//end handSubmitFav
- 
-  console.log(gif)
+        image_url: gif.images.fixed_height.url,
+      },
+    });
+    console.log(clicked);
+  }; //end handSubmitFav
 
   return (
     <div className="gifItem">
-      <h4>{gif.title}</h4>
-      <img src={gif.images.fixed_height.url} />
-      <button onClick={handleSubmitFav}>Save</button>
+      <Card sx={{ maxWidth: 345 }}>
+        <CardMedia
+          component="img"
+          alt={gif.title}
+          height="200"
+          image={gif.images.fixed_height.url}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {gif.title}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <IconButton
+            size="small"
+            aria-label="save"
+            onClick={handleSubmitFav}
+            color="error"
+          >
+            <FavoriteBorderOutlinedIcon />
+          </IconButton>
+          <Typography>Favorite</Typography>
+        </CardActions>
+      </Card>
     </div>
   );
 }
