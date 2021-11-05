2079c4b18da1add2491f368957dc0681272a8d4b
Hamsa Alimci <alimici2016@gmail.com>
2021-11-05 10:50:51 -0500

    Update on server is complete, The client side is not quite done yet

--- server/routes/favorite.router.js
+++ server/routes/favorite.router.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 const express = require('express');
 const pool = require('../modules/pool');
 
@@ -15,13 +16,33 @@ router.post('/', (req, res) => {
 
 // update given favorite with a category id
 router.put('/:favId', (req, res) => {
+  let favId = req.params.favId
+
+  let queryText = `
+  UPDATE "favorites" 
+  SET "category_id" = $1
+  WHERE "id" = $2;
+  `;
+
+  let category_id = req.body
+
+  let values = [category_id, favId]
+
+  pool.query(queryText, values)
+  .then((response) => {
+    console.log('UPDATED', response)
+    res.sendStatus(201)
+  }).catch((err) => {
+    console.log('Err in UPDATE', err)
+    res.sendStatus(500);
+  });
   // req.body should contain a category_id to add to this favorite image
-  res.sendStatus(200);
 });
 
 // delete a favorite
 router.delete('/', (req, res) => {
   res.sendStatus(200);
+
 });
 
 module.exports = router;
--- src/components/FavoriteItem/FavoriteItem.jsx
+++ src/components/FavoriteItem/FavoriteItem.jsx
@@ -9,6 +9,15 @@ function FavoriteItem({gif}) {
             {gif.name}
             <img title={gif.title} src={gif.image_url} />
             <button onClick={()=> dispatch({type:'DELETE_GIF', payload: gif.id})}>Delete Gif</button>
+            
+            <select name={category} id={category}>
+            <option value="funny">Funny</option>
+            <option value="cohort">Cohort</option>
+            <option value="cartoon">Cartoon</option>
+            <option value="nsfw">*Nsfw*</option>
+            <option value="meme">Meme</option>
+            </select>
+            {/* <button onClick={() => dispatch({type: 'CHANGE_CATEGORY', payload: })}>Category</button> */}
          </li>
         </>
     )
