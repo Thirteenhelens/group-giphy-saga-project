f7e5213b1591c7179a8a79adb828a4c00525422d
Julia Bayless <julia.allistair@gmail.com>
2021-11-05 09:49:56 -0500

    Search working

--- src/components/GiphyForm/GiphyForm.jsx
+++ src/components/GiphyForm/GiphyForm.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 
+
 function GiphyForm() {
   const dispatch = useDispatch();
   const [searchGif, setSearchGif] = useState("");
@@ -16,6 +17,7 @@ function GiphyForm() {
     setSearchGif("");
   };
 
+ 
   return (
     <div>
       <form onSubmit={handleSearch}>
